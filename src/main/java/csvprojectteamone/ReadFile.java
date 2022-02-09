c004de129ceb0494bf7f481c0004b50a46bfa052
Sami Zirak <samizirakdev@gmail.com>
2022-02-09 14:24:44 +0000

    Phase 1 - Employee Class, readCSV Method Wrap Up & Testing
    Created a class to represent an Employee object. The employee object contains variables that represent each column in a employees record (first name, salary etc.). Expanded further on the readCSV method and made the data that is read in be stored as an Employee and then in a HashMap using the employeeID as the key. Made it so the readCSV method excludes duplicates ready to catch them and store them elsewhere. Whilst developing this method further a TDD approach was used by developing tests to ensure exceptions are caught and some simple behavior's are tested. Further tests to be writen. Also made sure to implement two new test csv's for JUnit testing.

--- src/main/java/csvprojectteamone/ReadFile.java
+++ src/main/java/csvprojectteamone/ReadFile.java
@@ -3,25 +3,44 @@ package csvprojectteamone;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 public class ReadFile {
-
-    public static String readCSV(String filePath){
-        ArrayList<String[]> employeeList = new ArrayList<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath));){
-            String line;
-            while((line = br.readLine()) != null){
-                employeeList.add(line.split(","));
+    public static void readCSV(String filePath, HashMap<Integer, Employee> employeeHashMap) {
+        String line;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath));) {
+            String headerLine = reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",", 10);
+                if (parts.length == 10) {
+                    int key = Integer.parseInt(parts[0]);
+                    Employee e = new Employee(
+                            key,
+                            parts[1],
+                            parts[2],
+                            parts[3].charAt(0),
+                            parts[4],
+                            parts[5].charAt(0),
+                            parts[6],
+                            LocalDate.parse(parts[7],formatter),
+                            LocalDate.parse(parts[8],formatter),
+                            Double.parseDouble(parts[9]));
+                    employeeHashMap.put(key, e);
+                } else {
+                    System.out.println("ignoring line: " + line);
+                }
             }
-            for(String[] s:employeeList){
-                System.out.println(Arrays.toString(s));
+            int count = 0;
+            for (Integer key : employeeHashMap.keySet()) {
+                System.out.println(key + ":" + employeeHashMap.get(key));
+                count++;
             }
-        } catch (IOException e) {
+            System.out.println(count);
+        }catch (IOException | NullPointerException e) {
             e.printStackTrace();
         }
-        return null;
     }
 }
--- src/test/java/csvprojectteamone/ReadFileTests.java
+++ src/test/java/csvprojectteamone/ReadFileTests.java
@@ -1,35 +1,89 @@
 package csvprojectteamone;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static csvprojectteamone.ReadFile.readCSV;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReadFileTests {
+    @BeforeAll
+    static void setUp(){
 
-//    @Test
-//    @DisplayName("Given a CSV file, the method reads the file")
-//    public void givenCSVReadsTheFile(){
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Given a CSV file, the method outputs the file to the console")
-//    public void givenCSVOutputToConsole(){
-//
-//    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"Gideon, Sami, Suyash, Donovan | Gideon, Sami, Suyash, Donovan"},
-            delimiter = '|')
-    @DisplayName("Given a CSV file return the values")
-    public void givenCSVReturnTheValues(String file, String expected){
-
-        assertEquals(expected, readCSV("names.csv"));
     }
 
+    @Test
+    @DisplayName("Given an invalid filePath and a valid HashMap, the readCSV method will not throw a \"IOException\"")
+    public void readCSVDoesNotThrowIOException(){
+       HashMap<Integer, Employee> testHash = new HashMap<>();
+       Assertions.assertDoesNotThrow(() -> readCSV("invalidFile.csv",testHash));
+     }
+    @Test
+    @DisplayName("Given a filePath and HashMap that is null, the readCSV method will not throw a \"NullPointerException\"")
+    public void readCSVDoesNotThrowNullPointerException(){
+        Assertions.assertDoesNotThrow(() -> readCSV("EmployeeRecords.csv",null));
+    }
+    @Test
+    @DisplayName("Given an invalid filePath and HashMap that is null, the readCSV method will not throw a \"NullPointerException\" or \"IOException\"")
+    public void readCSVDoesNotThrowExceptions(){
+        Assertions.assertDoesNotThrow(() -> readCSV("invalidFile.csv", null));
+    }
+    // IN DEVELOPMENT
+//    @Test
+//    @DisplayName("Given a filePath and HashMap the readCSV method will read the file")
+//    public void readCSVReads(){
+//        HashMap<Integer, Employee> eHashActual = new HashMap<Integer,Employee>();
+//        readCSV("TestRecords.csv",eHashActual);
+//        HashMap<Integer, Employee> eHashExpected = generateTestHashMap();
+//
+//        //Map<Integer, Boolean> result = areEqualKeyValues(generateTestHashMap(),eHashActual);
+//
+//    }
+    @Test
+    @DisplayName("Given a filePath and HashMap the readCSV method will read the employeeId and set it as the HashMaps keyset")
+    public void readCSVSetsEmployeeIDAsKeySet(){
+        HashMap<Integer, Employee> eHashActual = new HashMap<>();
+        readCSV("TestRecords.csv",eHashActual);
+        HashMap<Integer, Employee> eHashExpected = generateTestHashMap();
+
+        Assertions.assertEquals(eHashExpected.keySet(), eHashActual.keySet());
+    }
+    @Test
+    @DisplayName("Given a filePath and HashMap the readCSV method will read the file and remove duplicates")
+    public void readCSVRemovesDuplicates(){
+        HashMap<Integer, Employee> eHashActual = new HashMap<>();
+        readCSV("TestDuplicateRecords.csv",eHashActual);
+
+        HashMap<Integer, Employee> eHashExpected = generateTestHashMapDuplicates();
+
+        Assertions.assertEquals(eHashExpected.keySet(), eHashActual.keySet());
+    }
+    private Map<Integer, Boolean> areEqualKeyValues(Map<Integer, Employee> first, Map<Integer, Employee> second) {
+        return first.entrySet().stream()
+                .collect(Collectors.toMap(e -> e.getKey(),
+                        e -> e.getValue().equals(second.get(e.getKey()))));
+    }
+    private HashMap<Integer,Employee> generateTestHashMap(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+        HashMap<Integer, Employee> eHashExpected = new HashMap<>();
+        eHashExpected.put(198429,new Employee(198429,"Mrs.","Serafina",'I',"Bumgarner",'F',"serafina.bumgarner@exxonmobil.com", LocalDate.parse("9/21/1982",formatter),LocalDate.parse("02/01/2008",formatter),69294));
+        eHashExpected.put(178566,new Employee(178566,"Mrs.","Juliette",'M',"Rojo",'F',"juliette.rojo@yahoo.co.uk", LocalDate.parse("05/08/1967",formatter),LocalDate.parse("06/04/2011",formatter),193912));
+        eHashExpected.put(647173,new Employee(647173,"Mr.","Milan",'F',"Krawczyk",'M',"milan.krawczyk@hotmail.com", LocalDate.parse("04/04/1980",formatter),LocalDate.parse("1/19/2012",formatter),123681));
+        eHashExpected.put(847634,new Employee(847634,"Mr.","Elmer",'R',"Jason",'M',"elmer.jason@yahoo.com", LocalDate.parse("04/09/1996",formatter),LocalDate.parse("5/28/2017",formatter),93504));
+        return  eHashExpected;
+    }
+    private HashMap<Integer,Employee> generateTestHashMapDuplicates(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+        HashMap<Integer, Employee> eHashExpected = new HashMap<>();
+        eHashExpected.put(198429,new Employee(198429,"Mrs.","Serafina",'I',"Bumgarner",'F',"serafina.bumgarner@exxonmobil.com", LocalDate.parse("9/21/1982",formatter),LocalDate.parse("02/01/2008",formatter),69294));
+        eHashExpected.put(647173,new Employee(647173,"Mr.","Milan",'F',"Krawczyk",'M',"milan.krawczyk@hotmail.com", LocalDate.parse("04/04/1980",formatter),LocalDate.parse("1/19/2012",formatter),123681));
+        return  eHashExpected;
+    }
 }
