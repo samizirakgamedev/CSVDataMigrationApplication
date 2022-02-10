d863593a29502d1676b13c0d8b700e595ab66c43
Sami Zirak <samizirakdev@gmail.com>
2022-02-10 21:30:29 +0000

    Phase 2 - DataVerification Class Bugfixes & Wrap Up
    Fixed bug with DataVerification class whereby it was not checking the Employee objects properly due to the nature of the checks. Cleaned up the class and commented it out.  Cleaned up FileReader class and removed any redundant/unwanted code.  Fixed a bug with FileWriter class where records were being written over on the first line.

--- src/main/java/csvprojectteamone/DataVerification.java
+++ src/main/java/csvprojectteamone/DataVerification.java
@@ -1,80 +1,88 @@
 package csvprojectteamone;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class DataVerification { //try catch blocks have been added throughout (with possible exceptions) for better performance.
-
+public class DataVerification {
+    // Try catch blocks have been added throughout (with possible exceptions) for better performance.
+    // Formatter for comparing date format of employees data.
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
-
-    private static boolean isEmpIdValid(String empID){ //CHECK THE TYPES
-        try {
-            Integer.parseInt(String.valueOf(empID));
-        } catch (NumberFormatException e)
-        {
-            return false;
-        }
-        return true;
-    }
-    private static boolean isNamePrefix(String namePrefix){
-        String[] namePrefixes = new String[] {"Mr.", "Ms.", "Mrs.", "Dr.", "Drs.", "Hon.", "Prof."};
-        for (String nPrefix : namePrefixes)
-        {
-            if(nPrefix.equals(namePrefix))
-                return true;
-        }return false;
-    }
-    private static boolean isFirstNameValid(String firstName){
-        return (firstName.matches("^[a-zA-Z]*$"));
-    }
-    private static boolean isMiddleInitialValid(String middleName){
-
-        return ((middleName.matches("^[A-Z]") || middleName.length() != 1));
-    }
-    private static boolean isLastNameValid(String lastName){
-        return (lastName.matches("^[A-Z]+(['-][A-Z][a-z[A-Z]]+)*$")); //This regex also accepts last names containing ' or - character on their index 1 (i.e. O'Neil or O-Neil )
-    }
-    private static boolean isGenderValid(String g){
-        return g.equals("M") || g.equalsIgnoreCase("F"); //Can also use gender.matches(regex) to make it case-sensitive (if needed).
-    }
-    private static boolean isEmailValid(String email){
-        return email.matches("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"); //Regular Expression by RFC 5322 for Email Validation. Can also use a basic one ^(.+)@(.+)$ whose main focus is just the @ sign.
-    }
-    private static boolean isDateOfBirthValid(String dateOfBirth){
+    // Checks to make sure the employees ID is valid by making sure it is an integer.
+    private static boolean isEmpIdValid(Integer empID){
         try{
-            formatter.parse(dateOfBirth);
-        } catch(Exception e){
-            return false;
-        }
-        return true;
-    }
-    private static boolean isDateOfJoiningValid(String dateOfJoining){
-        try{
-            formatter.parse(dateOfJoining);
-        } catch(Exception e){
-            return false;
-        }
-        return true;
-    }
-    private static boolean isSalaryValid(String salary){
-        try {
-            Integer.parseInt(salary);
+            Integer.valueOf(empID);
         } catch (NumberFormatException e) {
             return false;
         }
         return true;
     }
-    //call this method to verify an employee record.
-    public static boolean isEmployeeDataValid(Employee employee)
-    {
+    // Checks to make sure the employees name prefix is valid by looping through an array of prefixes.
+    private static boolean isNamePrefix(String namePrefix){
+        String[] namePrefixes = new String[] {"Mr.", "Ms.", "Mrs.", "Dr.", "Drs.", "Hon.", "Prof."};
+        for (String nPrefix : namePrefixes) {
+            if(nPrefix.equals(namePrefix))
+                return true;
+        }
+        return false;
+    }
+    // Checks to make sure the employees first name is valid using regex.
+    private static boolean isFirstNameValid(String firstName){
+        return (firstName.matches("^[a-zA-Z]*$"));
+    }
+    // Checks to make sure the employees middle initial is a valid char.
+    private static boolean isMiddleInitialValid(Character middleName){
+        return ((middleName >= 'A' && middleName <= 'Z'));
+    }
+    // Checks to make sure the employees last name is valid using regex.
+    private static boolean isLastNameValid(String lastName){
+        return (lastName.matches("^[a-zA-Z]*$")); //This regex also accepts last names containing ' or - character on their index 1 (i.e. O'Neil or O-Neil )
+    }
+    // Checks to make sure the employees gender char is valid by checking if it equals 'M' of 'F'.
+    private static boolean isGenderValid(Character g){
+        return g.equals('M') || g.equals('F');
+    }
+    // Checks to make sure the employees email is valid using regex for email addresses.
+    private static boolean isEmailValid(String email){
+        return email.matches("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
+    }
+    // Checks to make sure the employees date of birth is valid checking if the formatter can format the passed in date.
+    private static boolean isDateOfBirthValid(LocalDate dateOfBirth){
+        try{
+            formatter.format(dateOfBirth);
+        } catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+    // Checks to make sure the employees date of joining is valid checking if the formatter can format the passed in date.
+    private static boolean isDateOfJoiningValid(LocalDate dateOfJoining){
+        try{
+            formatter.format(dateOfJoining);
+        } catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+    // Checks to make sure the employee's salary is valid by making sure it is a double.
+    private static boolean isSalaryValid(Double salary){
+        try {
+            Double.valueOf(salary);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+    // Method to call to verify if the 'Employee' object as a whole is valid.
+    public static boolean isEmployeeDataValid(Employee employee){
         return (isEmpIdValid(employee.getEmployeeId())
-                && isNamePrefix(employee)
-                && isFirstNameValid(employee[2])
-                && isMiddleInitialValid(employee[3])
-                && isLastNameValid(employee[4])
-                && isGenderValid(employee[5])
-                && isEmailValid(employee[6])
-                && isDateOfBirthValid(employee[7])
-                && isDateOfJoiningValid(employee[8])
-                && isSalaryValid(employee[9]));
-    } //can store the length of the data column in the enum class or a separate constant class
+                && isNamePrefix(employee.getTitle())
+                && isFirstNameValid(employee.getFirstName())
+                && isMiddleInitialValid(employee.getMiddleInitial())
+                && isLastNameValid(employee.getLastName())
+                && isGenderValid(employee.getGender())
+                && isEmailValid(employee.getEmailAddress())
+                && isDateOfBirthValid(employee.getDateOfBirth())
+                && isDateOfJoiningValid(employee.getDateOfJoining())
+                && isSalaryValid(employee.getSalary()));
+    }
 }
--- src/main/java/csvprojectteamone/FileReaderClass.java
+++ src/main/java/csvprojectteamone/FileReaderClass.java
@@ -5,20 +5,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class FileReaderClass {
-    //need DAO Class to complete this class, so that we can store duplicate and corrupt records in a separate file
-    //Under construction
-    //private static final HashMap<String, Employee> records = new HashMap<>();
-
     public static void readCSV(String filePath, HashMap<Integer, Employee> employeeHashMap) {
         String line;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath));) {
             String headerLine = reader.readLine();
-
             while ((line = reader.readLine()) != null)
             {
                 String[] parts = line.split(",");
@@ -36,58 +30,26 @@ public class FileReaderClass {
                         LocalDate.parse(parts[7],formatter),
                         LocalDate.parse(parts[8],formatter),
                         Double.parseDouble(parts[9]));
-                //employeeHashMap.put(key, employee);
-//
-                if(DataVerification.isEmployeeDataValid(employee)) //check!!
+                if(DataVerification.isEmployeeDataValid(employee))
                 {
-                    if(employeeHashMap.containsKey(key))
-                    {
+                    if(!employeeHashMap.containsKey(key))
                         employeeHashMap.put(key, employee);
-                    }
                     else
-                    {
                         FileWriterClass.writeToCSVFile("DuplicateRecords.csv", employee, "Duplicate-data");
-                    }
                 } else
-
-                {
                     FileWriterClass.writeToCSVFile("CorruptRecords.csv", employee, "Corrupt-data");
-                }
             }
-
-
-
-//                if (parts.length == 10) {
-//                    int key = Integer.parseInt(parts[0]);
-//                    Employee e = new Employee(
-//                            key,
-//                            parts[1],
-//                            parts[2],
-//                            parts[3].charAt(0),
-//                            parts[4],
-//                            parts[5].charAt(0),
-//                            parts[6],
-//                            LocalDate.parse(parts[7],formatter),
-//                            LocalDate.parse(parts[8],formatter),
-//                            Double.parseDouble(parts[9]));
-//                    employeeHashMap.put(key, e);
-//                } else {
-//                    System.out.println("ignoring line: " + line);
-//                }
-//            }
-
-
-            //give the command to insert the filtered data to a database
-
-            int count = 0;
-            for (Integer key : employeeHashMap.keySet()) {
-                System.out.println(key + ":" + employeeHashMap.get(key));
-                count++;
-            }
-            System.out.println(count);
+            outputRecords(employeeHashMap);
         }catch (IOException | NullPointerException e) {
             e.printStackTrace();
         }
     }
-
+    public static void outputRecords(HashMap<Integer,Employee> hash){
+        int count = 0;
+        for (Integer key : hash.keySet()) {
+            System.out.println(key + ":" + hash.get(key));
+            count++;
+        }
+        System.out.println(count);
+    }
 }
