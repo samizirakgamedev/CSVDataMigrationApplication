973b3a349af8eaa888b250874fd754dd978b0d7a
Sami Zirak <samizirakdev@gmail.com>
2022-02-11 12:34:29 +0000

    Phase 1 - Implementation Of User Interface
    Implemented some basic methods for calling to build a user interface for the user. This will be built upon once the remaining implementation has been complete and we know what getters and setters we will be using to read and modify data.

--- src/main/java/csvprojectteamone/CSVDataMigrationMain.java
+++ src/main/java/csvprojectteamone/CSVDataMigrationMain.java
@@ -7,7 +7,12 @@ import static csvprojectteamone.ReadFile.readCSV;
 
 public class CSVDataMigrationMain {
     public static void main(String[] args) {
-        HashMap<Integer, Employee> ourNames = new HashMap<Integer,Employee>();
-        readCSV("EmployeeRecords.csv",ourNames);
+        //HashMap<Integer, Employee> ourNames = new HashMap<Integer,Employee>();
+        //readCSV("EmployeeRecords.csv",ourNames);
+        DisplayManager displayManager = new DisplayManager();
+
+        displayManager.getStringFromUser(
+                "Please enter the filepath of the CSV file you want to read and press \'Enter\':",
+                "| E.g.\"EmployeeRecords.csv\" |");
     }
 }
---
+++ src/main/java/csvprojectteamone/DisplayManager.java
@@ -0,0 +1,43 @@
+package csvprojectteamone;
+
+import java.util.Scanner;
+
+public class DisplayManager {
+    // For Output.
+    StringBuilder builder = new StringBuilder();
+    // For Input.
+    Scanner scanner = new Scanner(System.in);
+    // String for separating the console outputs.
+    String separateConsoleOutput = "==================================";
+
+    public void outputReadAndWriteStatus(){
+        // Output that the reading of the file was successful.
+        // Output that the writing to the database was successful.
+    }
+    public void outputFileStats(){
+        System.out.println("");
+        // Output: How many duplicates and corrupt data (getDuplicates)
+        // Output: How many records were added to the database (getRecordCount)
+    }
+    public void outputAvailableOptions(){
+        System.out.println("");
+        // Option for getting employee via ID from the database.
+        // Option to view all duplicate and corrupt data from the database.
+    }
+    public String getStringFromUser(String message, String messageTwo){
+        System.out.println(message);
+        if (messageTwo != null)
+            System.out.println(messageTwo);
+        String inputString = scanner.next();
+        System.out.println(separateConsoleOutput);
+        return inputString;
+    }
+    public int getIntFromUser(String message, String messageTwo){
+        System.out.println(message);
+        if (messageTwo != null)
+            System.out.println(messageTwo);
+        int inputInt = scanner.nextInt();
+        System.out.println(separateConsoleOutput);
+        return inputInt;
+    }
+}
